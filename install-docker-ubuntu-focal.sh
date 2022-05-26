169c01d846461dec722fd89ade298fbb30df51d5
Marcos Aurélio Silvério <maureliosilverio@gmail.com>
2022-05-26 19:14:45 -0300

    Create install-docker-ubuntu-focal.sh

---
+++ install-docker-ubuntu-focal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt update
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update && apt install -y docker-ce
+systemctl enable docker && systemctl start docker
