22158502d9f8ca4b0deec09abb5ab2b14f7d0c0d
Marcos Aurélio Silvério <maureliosilverio@gmail.com>
2022-05-26 19:48:48 -0300

    Update install-minikube-ubuntu-focal.sh

--- install-minikube-ubuntu-focal.sh
+++ install-minikube-ubuntu-focal.sh
@@ -3,6 +3,8 @@ curl -o /usr/local/bin/minikube https://storage.googleapis.com/minikube/releases
 chmod +x /usr/local/bin/minikube
 curl -o /usr/local/bin/kubectl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x /usr/local/bin/kubectl
-useradd -s /bin/bash minikube
+mkdir /home/minikube
+useradd -s /bin/bash -d /home/minikube minikube
 usermod -aG docker minikube
+chown minikube /home/minikube
 echo "Done! Next step is log in with minikube user and start with "minikube start" command"
